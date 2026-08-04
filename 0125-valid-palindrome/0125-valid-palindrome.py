3b71c56b07548e682fd3cb348954c682ac3a1105
R Gaurav <gaurav.vfx123@gmail.com>
2026-08-04 22:51:07 +0530

    Time: 283 ms (5.01%), Space: 13.1 MB (44.68%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        filtered = ""
+
+        for ch in s:
+            if ch.isalnum():
+                filtered += ch.lower()
+
+        return filtered == filtered[::-1]
