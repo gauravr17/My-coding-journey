e6a394edf95a190a2cf7775a5dafc981a4352627
R Gaurav <gaurav.vfx123@gmail.com>
2026-08-06 01:28:53 +0530

    Time: 47 ms (26.36%), Space: 12.4 MB (99.03%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        note_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+        
+        for char, count in note_count.items():
+            if magazine_count[char] < count:
+                return False
+        
+        return True
