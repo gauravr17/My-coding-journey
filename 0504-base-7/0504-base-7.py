d9d99f8a133659ed60cfc81e4fbc4665d79673cc
R Gaurav <gaurav.vfx123@gmail.com>
2026-08-07 21:41:40 +0530

    Time: 2 ms (8.61%), Space: 12.3 MB (88.76%) - LeetHub

---
+++ 0504-base-7/0504-base-7.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+            return "0"
+        
+        negative = num < 0
+        num = abs(num)
+        digits = []
+        
+        while num > 0:
+            digits.append(str(num % 7))
+            num //= 7
+        
+        if negative:
+            digits.append("-")
+        
+        return "".join(reversed(digits))
