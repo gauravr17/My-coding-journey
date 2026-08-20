923ef75d76224a2114af37a999fd8e5656667fff
R Gaurav <gaurav.vfx123@gmail.com>
2026-08-21 00:00:52 +0530

    Time: 261 ms (47.3%), Space: 131.3 MB (26.72%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        
+        def backtrack(start, path):
+            if len(path) == k:
+                result.append(path[:])
+                return
+            
+            for num in range(start, n + 1):
+                path.append(num)
+                backtrack(num + 1, path)
+                path.pop()  # undo the choice, try the next number
+        
+        backtrack(1, [])
+        return result
